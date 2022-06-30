be613443525ad2a7a88163742730b98fd01d5b76
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-06-30 17:00:09 +0100

    plan written for the counter

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, EventEmitter } from "@angular/core";
+import { UsersService } from "./users.services";
 
 @Injectable({providedIn: 'root'})
 
 export class CounterService {
+
+  constructor(private usersService: UsersService) {}
+
   activeToInactive = 0;
 
   inactiveToActive = 0;
 }
+
+//when the usersSetToInactive event emitter from the UsersService is called, add 1 to the activeToInactive variable.
+//when the usersSetToActive event emitter from the UsersService is called, add 1 to the activeToActive variable.

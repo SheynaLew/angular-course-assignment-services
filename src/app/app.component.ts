00e2f67f9e02bcaabb1a915e1bdd509e447ddb5a
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-06-30 16:11:20 +0100

    altered the functions in the app component to use the array in the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from './services/users.services';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+  // activeUsers = ['Max', 'Anna'];
+  // inactiveUsers = ['Chris', 'Manu'];
+
+  constructor(private usersService: UsersService) {};
 
   onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+    // this.inactiveUsers.push(this.activeUsers[id]);
+    this.usersService.inactiveUsers.push(this.usersService.activeUsers[id]);
+    // this.activeUsers.splice(id, 1);
+    this.usersService.activeUsers.splice(id, 1);
   }
 
   onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+    // this.activeUsers.push(this.inactiveUsers[id]);
+    this.usersService.activeUsers.push(this.usersService.inactiveUsers[id]);
+    // this.inactiveUsers.splice(id, 1);
+    this.usersService.inactiveUsers.splice(id, 1);
   }
 }

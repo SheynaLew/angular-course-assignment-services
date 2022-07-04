0a32df644e5a56912699a24305268ff519004451
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-07-04 15:54:10 +0100

    got the counter working on my own

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -5,11 +5,21 @@ import { UsersService } from "./users.services";
 
 export class CounterService {
 
-  constructor(private usersService: UsersService) {}
+  // constructor(private usersService: UsersService) {}
 
   activeToInactive = 0;
 
   inactiveToActive = 0;
+
+  incrementInactive(n: number) {
+    this.activeToInactive+=n;
+    console.log("active to inactive in counter service", this.activeToInactive);
+  }
+
+  incrementActive(n: number) {
+    this.inactiveToActive+=n;
+    console.log("inactive to active in counter service", this.inactiveToActive);
+  }
 }
 
 //when the usersSetToInactive event emitter from the UsersService is called, add 1 to the activeToInactive variable.
--- src/app/services/users.services.ts
+++ src/app/services/users.services.ts
@@ -1,21 +1,38 @@
-import { Injectable, EventEmitter } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { CounterService } from "./counter.service";
 
 @Injectable({providedIn: 'root'})
 
 export class UsersService {
+  constructor(private counterService: CounterService)  {};
 
- activeUsers = ['Max', 'Anna'];
+  // switchedToInactive: number = 0;
+  // switchedToActive: number = 0;
 
- inactiveUsers = ['Chris', 'Manu'];
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
 
- setToInactive(id: number) {
-  this.inactiveUsers.push(this.activeUsers[id]);
-  this.activeUsers.splice(id, 1);
+
+  setToInactive(id: number, n: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+    n = 1;
+    this.counterService.incrementInactive(n);
+
+
+    // this.counterService.activeToInactive = this.counterService.activeToInactive + 1;
+    // console.log("activeToInactive", this.counterService.activeToInactive);
+
 }
 
-  setToActive(id: number) {
+  setToActive(id: number, n: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    n = 1;
+    this.counterService.incrementActive(n);
+
+    // this.counterService.inactiveToActive = this.counterService.inactiveToActive + 1;
+    // console.log("inactiveToActive", this.counterService.inactiveToActive);
 }
 
 }
